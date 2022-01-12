d0918ac0f4922b1d89ad83d4353488065ffe92a7
theGetz1995 <71470135+theGetz1995@users.noreply.github.com>
2022-01-12 11:23:55 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using data to determine what actions have the largest impact on ranking up or down in Starcraft 2',
+    author='Jacob Getz',
+    license='MIT',
+)
